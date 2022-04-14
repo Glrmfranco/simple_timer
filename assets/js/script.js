a72f25ff5a9ecd3f0cf8f806493109635880a5a6
Guilherme Franco <glrmfranco@gmail.com>
2022-04-14 16:10:54 -0300

    ;

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,34 @@
-const relogio = document.querySelector('.relogio')
-let segundos = 0
-let timer
+const relogio = document.querySelector('.relogio');
+let segundos = 0;
+let timer;
 
 function getHora(segundos) {
-    const data = new Date(segundos * 1000)
-    return data.toLocaleTimeString('pt-BR', { hour12: false, timeZone: 'UTC' })
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', { hour12: false, timeZone: 'UTC' });
 }
 
 function iniciaRelogio() {
     timer = setInterval(() => {
-        segundos++
-        relogio.innerHTML = getHora(segundos)
+        segundos++;
+        relogio.innerHTML = getHora(segundos);
     }, 1000);
 }
 
 document.addEventListener('click', (event) => {
-    const et = event.target
+    const et = event.target;
     if (et.classList.contains('iniciar')) {
-        clearInterval(timer)
-        iniciaRelogio()
-        relogio.classList.remove('pausado')
+        clearInterval(timer);
+        iniciaRelogio();
+        relogio.classList.remove('pausado');
     }
     if (et.classList.contains('pausar')) {
-        clearInterval(timer)
-        relogio.classList.add('pausado')
+        clearInterval(timer);
+        relogio.classList.add('pausado');
     }
     if (et.classList.contains('zerar')) {
-        clearInterval(timer)
-        segundos = 0
-        relogio.innerHTML = '00:00:00'
-        relogio.classList.remove('pausado')
+        clearInterval(timer);
+        segundos = 0;
+        relogio.innerHTML = '00:00:00';
+        relogio.classList.remove('pausado');
     }
-})
+});
